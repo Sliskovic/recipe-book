21a5df583ab1387db94020fcb8fdf4d22cc41b79
Mara Sliskovic <sliskovicmara92@gmail.com>
2022-03-17 15:30:13 +0100

    child routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,13 @@ import { HeaderComponent } from "./header/header.component";
 import { RecipesComponent } from "./recipes/recipes.component";
 import { RecipeListComponent } from "./recipes/recipe-list/recipe-list.component";
 import { RecipeDetailComponent } from "./recipes/recipe-detail/recipe-detail.component";
-import { RecipeItemComponent } from "./recipes/recipe-list/recipe-item/recipe-item.component";
+import { RecipeItemComponent } from "./recipes/recipe-item/recipe-item.component";
 import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
 import { ShoppingEditComponent } from "./shopping-list/shopping-edit/shopping-edit.component";
 import { DropdownDirective } from "./shared/dropdown.directive";
 import { ShoppingListService } from "./shopping-list/shopping-list.service";
 import { AppRoutingModule } from "./app-routing.module";
+import { RecipeStartComponent } from "./recipes/recipe-start/recipe-start.component";
 
 @NgModule({
   declarations: [
@@ -25,6 +26,7 @@ import { AppRoutingModule } from "./app-routing.module";
     ShoppingListComponent,
     ShoppingEditComponent,
     DropdownDirective,
+    RecipeStartComponent,
   ],
   imports: [BrowserModule, FormsModule, AppRoutingModule],
   providers: [ShoppingListService],
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -11,13 +11,13 @@ export class RecipeService {
     new Recipe(
       "Tasty Schnitzel",
       "A super-tasty Schnitzel - just awesome!",
-      "https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG",
+      "https://www.carolinescooking.com/wp-content/uploads/2016/06/Wiener-schnitzel-photo-1.jpg",
       [new Ingredient("Meat", 1), new Ingredient("French Fries", 20)]
     ),
     new Recipe(
       "Big Fat Burger",
       "What else you need to say?",
-      "https://upload.wikimedia.org/wikipedia/commons/b/be/Burger_King_Angus_Bacon_%26_Cheese_Steak_Burger.jpg",
+      "https://kbr.com.pk/wp-content/uploads/2020/08/burger.jpg",
       [new Ingredient("Buns", 2), new Ingredient("Meat", 1)]
     ),
   ];
