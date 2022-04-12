f4cb5fcc995f34cb2cee77fc733a0b66ab7f8b00
Mara Sliskovic <sliskovicmara92@gmail.com>
2022-04-12 16:34:27 +0200

    http

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -9,26 +9,33 @@ export class RecipeService {
  
   recipesChange = new Subject<Recipe[]>();
 
-  private recipes: Recipe[] = [
-    new Recipe(
-      "Tasty Schnitzel",
-      "A super-tasty Schnitzel - just awesome!",
-      "https://www.carolinescooking.com/wp-content/uploads/2016/06/Wiener-schnitzel-photo-1.jpg",
-      [
-        new Ingredient("Meat", 1), 
-        new Ingredient("French Fries", 20)
-      ]),
-    new Recipe(
-      "Big Fat Burger",
-      "What else you need to say?",
-      "https://www.thespruceeats.com/thmb/ad4-cZdHW0fHWDn_JSuh3FTjLag=/940x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/garlic-burger-patties-333503-hero-01-e4df660ff27b4e5194fdff6d703a4f83.jpg",
-      [
-        new Ingredient("Buns", 2), 
-        new Ingredient("Meat", 1)
-      ]),
-  ];
+  // private recipes: Recipe[] = [
+  //   new Recipe(
+  //     "Tasty Schnitzel",
+  //     "A super-tasty Schnitzel - just awesome!",
+  //     "https://www.carolinescooking.com/wp-content/uploads/2016/06/Wiener-schnitzel-photo-1.jpg",
+  //     [
+  //       new Ingredient("Meat", 1), 
+  //       new Ingredient("French Fries", 20)
+  //     ]),
+  //   new Recipe(
+  //     "Big Fat Burger",
+  //     "What else you need to say?",
+  //     "https://www.thespruceeats.com/thmb/ad4-cZdHW0fHWDn_JSuh3FTjLag=/940x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/garlic-burger-patties-333503-hero-01-e4df660ff27b4e5194fdff6d703a4f83.jpg",
+  //     [
+  //       new Ingredient("Buns", 2), 
+  //       new Ingredient("Meat", 1)
+  //     ]),
+  // ];
+
+  private recipes: Recipe[] = [];
 
   constructor(private shoppingListService: ShoppingListService) {}
+
+  setRecipes(recipes: Recipe[]){
+    this.recipes = recipes;
+    this.recipesChange.next(this.recipes.slice())
+  }
 
   getRecipes() {
     return this.recipes.slice();
